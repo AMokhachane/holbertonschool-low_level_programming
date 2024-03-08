1cc61419f1871e5089b823bfd2f54c0a3a3501a7
AMokhachane <AMokhachane@singular.co.za>
2024-03-08 13:14:26 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,21 +1,11 @@
 #include "main.h"
 
 /**
- * main - function
- * Return: 0
+ * main - check the code
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	print_alphabet();
 	return (0);
-
 }

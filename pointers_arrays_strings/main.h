079a2b7da3d8fde3cce21ef8f236fbcef6b9834d
AMokhachane <AMokhachane@singular.co.za>
2024-03-18 09:49:16 +0000

    1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,35 +1,17 @@
-[11:29] Nhlayiseko Bvuma
-#include "main.h"
- 
-/**
+#ifndef MAIN_H
+#define MAIN_H
 
-* print_chessboard - Prints the chessboard.
+#include <stdio.h>
 
-* @a: The chessboard array.
-
-*/
-
-void print_chessboard(char (*a)[8])
-
-{
-
-    int i, j;
- 
-    for (i = 0; i < 8; i++)
-
-    {
-
-        for (j = 0; j < 8; j++)
-
-        {
-
-            _putchar(a[i][j]);
-
-        }
-
-        _putchar('\n');
-
-    }
-
-}
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+#endif
 

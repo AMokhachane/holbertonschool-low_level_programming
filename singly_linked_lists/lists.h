4a41cbb4826c1df9c001ea79f73fc0618eca1eab
AMokhachane <AMokhachane@singular.co.za>
2024-04-17 12:23:52 +0000

    lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,24 +1,28 @@
-#ifndef LINKED
-#define LINKED
-
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef _LINKED_LIST
+#define _LINKED_LIST
 
 /**
- * description
- */
-typedef struct lists_s
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+* for Holberton project
+*/
+typedef struct list_s
 {
 char *str;
 unsigned int len;
-struct lists_s *next;
-} lists_t
+struct list_s *next;
+} list_t;
 
-size_t print_list(const lists_t *h);
-size_t lists_hen(const lists_t *h);
-lists_t *add_node(lists_t **head, const char *str);
-lists_t *add_node_end(lists_t **head, const char *str);
-void free_lists(lists_t *head);
+int _putchar(char c);
+int _strlen(const char *s);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-#endif /*LINKED*/
+#endif
